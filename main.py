00b2271274f5a4ac9a2a2d282a146dd39e35266c
Lorenzo Hernandez-Nunez <113930435+Lorenzo1135@users.noreply.github.com>
2023-02-16 18:40:24 +0000

    coded

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# remember to fork this and push to github
+# # remember to fork this and push to github
 
+
+# f = open('employees.txt', 'r')
+# f.write("you thought i was feeling you?")
+# print(f.read())
+# #read gives us everything.readline reads first line
+
+with open("test.txt" , 'r') as f:
+  for line in f:
+    print(line,end='')
+  # # f.read()
+  # print(f)
